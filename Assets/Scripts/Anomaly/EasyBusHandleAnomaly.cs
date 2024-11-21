3141d27a02ccc0e59b385956061147b7b508d5f9
Gunhee Kim <gunhee2001@snu.ac.kr>
2024-11-21 20:39:21 +0900

    implement anomaly for four bus handle

--- Assets/Scripts/Anomaly/EasyBusHandleAnomaly.cs
+++ Assets/Scripts/Anomaly/EasyBusHandleAnomaly.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class EasyBusHandleAnomaly : Anomaly
 {
+    GameObject[] busHandles;
     public override void Apply(GameObject map)
     {
-        GameObject busHandle = map.transform.Find("Interior").Find("2nd Floor").Find("Apartment_01").Find("Props").Find("bus_handle").gameObject;
-        GameObject animationController = new GameObject("AnomalyAnimationController");
-        BusHandleAnomalyAnimationController anomalyAnimationController = animationController.AddComponent<BusHandleAnomalyAnimationController>();
-        float duration = 300.0f;
-        anomalyAnimationController.playAnimation(busHandle, duration);
-        GameObject.Destroy(animationController, duration);
+        busHandles = new GameObject[4];
+        for (int i = 0; i < 4; i++)
+        {
+            busHandles[i] = map.transform.Find("Interior").Find("2nd Floor").Find("Apartment_01").Find("Props").Find("bus_handle (" + i + ")").gameObject;
+        }
+        foreach (GameObject busHandle in busHandles)
+        {
+            GameObject animationController = new GameObject("AnomalyAnimationController");
+            BusHandleAnomalyAnimationController anomalyAnimationController = animationController.AddComponent<BusHandleAnomalyAnimationController>();
+            float duration = 300.0f;
+            anomalyAnimationController.playAnimation(busHandle, duration);
+            GameObject.Destroy(animationController, duration);
+        }
     }
 }
