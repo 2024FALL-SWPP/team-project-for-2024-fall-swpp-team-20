086b8d0faf801f48aa4b5e84c4f9c63abf07c66f
Gunhee Kim <gunhee2001@snu.ac.kr>
2024-11-26 09:48:24 +0900

    feat : implement EasyDrawerMissingContentAnomaly

---
+++ Assets/Scripts/Anomaly/EasyDrawerMissingContentAnomaly.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+class EasyDrawerMissingContentAnomaly : Anomaly
+{
+    public override void Apply(GameObject map)
+    {
+        GameObject drawer = map.transform.Find("Interior/2nd Floor/Apartment_01/Furniture/Table_Computer_01_Setup").gameObject;
+        GameObject missingContent = drawer.transform.Find("Drawer_R_01/tissue").gameObject;
+        missingContent.SetActive(false);
+    }
+}
