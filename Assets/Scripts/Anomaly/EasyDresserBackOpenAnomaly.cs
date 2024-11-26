cbfa353451daa46b9254b9bf4993c1e61c0319db
Gunhee Kim <gunhee2001@snu.ac.kr>
2024-11-26 09:42:31 +0900

    feat : implement EasyDresserBackOpenAnomaly

---
+++ Assets/Scripts/Anomaly/EasyDresserBackOpenAnomaly.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+class EasyDresserBackOpenAnomaly : Anomaly
+{
+    public override void Apply(GameObject map)
+    {
+        GameObject dresser = map.transform.Find("Interior/2nd Floor/Apartment_01/Furniture/Dresser Closet_01").gameObject;
+        GameObject backOpenedDresser = map.transform.Find("Interior/2nd Floor/Apartment_01/Furniture/Dresser Closet_01_BackOpened").gameObject;
+        dresser.SetActive(false);
+        backOpenedDresser.SetActive(true);
+    }
+}
