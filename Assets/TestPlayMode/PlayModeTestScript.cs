cbe44fa9a1037b014d2a48351a6f0b3a1be4d33c
dandyday <dandyday31@gmail.com>
2024-12-05 15:29:17 +0900

    test: added test for previous commit

--- Assets/TestPlayMode/PlayModeTestScript.cs
+++ Assets/TestPlayMode/PlayModeTestScript.cs
@@ -44,8 +44,55 @@ public class PlayModeTestScript
         ObjectStorage objectStorage = GameObject.FindObjectOfType<ObjectStorage>();
 
         Assert.IsNotNull(objectStorage, "ObjectStorage가 로드되지 않았습니다.");
-        Assert.IsNotNull(objectStorage.chessHitbox, "chessHitbox를 레퍼런스하고 있지 않습니다.");
-        Assert.IsNotNull(objectStorage.chessWalls, "chessWalls를 레퍼런스하고 있지 않습니다.");
+
+        Assert.IsTrue(objectStorage.bookCollection.Length == 2, "bookCollection");
+        Assert.IsTrue(objectStorage.busHandles.Length == 4, "busHandles");
+
+        Assert.IsNotNull(objectStorage.normalCanvas, "normalCanvas");
+        Assert.IsNotNull(objectStorage.anomalCanvas, "anomalCanvas");
+
+        Assert.IsNotNull(objectStorage.giraffeCanvas, "giraffeCanvas");
+
+        Assert.IsNotNull(objectStorage.normalCanvasFlip, "normalCanvasFlip");
+
+        Assert.IsNotNull(objectStorage.normalCube, "normalCube");
+        Assert.IsNotNull(objectStorage.anomalyCube, "anomalyCube");
+
+        Assert.IsNotNull(objectStorage.normalDice, "normalDice");
+        Assert.IsNotNull(objectStorage.anomalyDice, "anomalyDice");
+
+        Assert.IsNotNull(objectStorage.digitalClockText, "digitalClockText");
+
+        Assert.IsNotNull(objectStorage.drawerMissingContent, "drawerMissingContent");
+
+        Assert.IsNotNull(objectStorage.dresser, "dresser");
+        Assert.IsNotNull(objectStorage.backOpenedDresser, "backOpenedDresser");
+
+        Assert.IsTrue(objectStorage.hangers.Length == 4, "hangers");
+
+        Assert.IsNotNull(objectStorage.laptopObject, "laptopObject");
+
+        Assert.IsNotNull(objectStorage.anomalyLight, "anomalyLight");
+
+        Assert.IsNotNull(objectStorage.pianoObject, "pianoObject");
+
+        Assert.IsNotNull(objectStorage.playerAwake, "playerAwake");
+        Assert.IsNotNull(objectStorage.playerSleeping, "playerSleeping");
+
+        Assert.IsNotNull(objectStorage.sofa, "sofa");
+
+        Assert.IsNotNull(objectStorage.spintopObject, "spintopObject");
+
+        Assert.IsNotNull(objectStorage.normalTeddyBear, "normalTeddyBear");
+        Assert.IsNotNull(objectStorage.anomalyTeddyBear, "anomalyTeddyBear");
+
+        Assert.IsNotNull(objectStorage.chessHitbox, "chessHitbox");
+        Assert.IsNotNull(objectStorage.chessWalls, "chessWalls");
+
+        Assert.IsNotNull(objectStorage.bed, "bed");
+
+        Assert.IsNotNull(objectStorage.digitalClock, "digitalClock");
+        Assert.IsNotNull(objectStorage.timeBomb, "timeBomb");
     }
 
 }
