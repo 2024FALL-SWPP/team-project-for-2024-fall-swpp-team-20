ccfef6d6dc5c95493a8a7e9be74fddd32d2bdf5e
junu123456 <junu123456@naver.com>
2024-12-16 15:03:38 +0900

    made achievement class

---
+++ Assets/Scripts/Managers/AchievementManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Achievements
+{
+    // Anomaly Clear
+    EasyBookColorAnomalyClear,
+    EasyBusHandleAnomalyClear,
+    EasyCanvasChangeAnomalyClear,
+    EasyCanvasDisappearAnomalyClear,
+    EasyCanvasFlipAnomalyClear,
+    EasyCubeAnomalyClear,
+    EasyDiceAnomalyClear,
+    EasyDigitalClockAnomalyClear,
+    EasyDrawerMissingContentAnomalyClear,
+    EasyDresserBackOpenAnomalyClear,
+    EasyHangerDisaeppearAnomalyClear,
+    EasyLaptopAnomalyClear,
+    EasyLightAnomalyClear,
+    EasyPianoAnomalyClear,
+    EasyPlayerAnomalyClear,
+    EasySofaDisasppearAnomalyClear,
+    EasySpintopAnomalyClear,
+    EasyTeddyBearAnomalyClear,
+    HardChessAnomalyClear,
+    HardFruitDropAnomalyClear,
+    HardLavaAnomalyClear,
+    HardReverseMapAnomalyClear,
+    HardTimeBombAnomalyClear,
+    HardVisibilityAnomalyClear,
+
+    GameClear,
+    GameClearWithoutMiss, // Game Cleared without any incorrect decision
+    GameFail, // Game over
+
+    ClearAllAnomaly, // find and clear all anomalies
+    LongGame, // meet 15 stage in one game
+
+    ChessAnomalyClearWithNoDamage, // Clear Chess Anomaly without taking any damage
+    LavaAnomalyClearFast, // Clear Lava Anomaly within 20 seconds
+    VisibilityAnomalyClearFast, // Clear Visibility Anomaly within 20 seconds
+}
+
+public class AchievementManager : MonoBehaviour
+{
+    public void ShowClearPanel(Achievements achievement)
+    {
+
+    }
+
+    private int achievementFlag = -1;
+
+    public void Initialize()
+    {
+        if (achievementFlag < 0) achievementFlag = 0;
+    }
+
+    public void ClearAchievement(Achievements achievement)
+    {
+        int flag = (int)Mathf.Pow(2, (int)achievement);
+
+        if ((achievementFlag & flag) == 0) achievementFlag += (int)Mathf.Pow(2, (int)achievement);
+    }
+}
