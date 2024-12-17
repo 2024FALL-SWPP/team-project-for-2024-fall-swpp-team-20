d037f2bc1567dc5ac13c9870e1554bea77f2192b
junu123456 <junu123456@naver.com>
2024-12-17 19:50:41 +0900

    BGM/SFX 분리

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -29,7 +29,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Settings")]
     private float sensitivity;
-    private float volume;
+    private float BGMVolume;
+    private float SFXVolume;
 
     private bool started;
 
@@ -61,7 +62,8 @@ public class GameManager : MonoBehaviour
     private void InitializeSetting()
     {
         sensitivity = 50f;
-        volume = 1f;
+        BGMVolume = 1f;
+        SFXVolume = 1f;
         achievementFlag = -1;
         started = false;
     }
@@ -125,14 +127,22 @@ public class GameManager : MonoBehaviour
 
     public GameState GetState() => state;
 
-    public void SetVolume(float value)
+    public void SetBGMVolume(float value)
     {
-        volume = value / 100f;
+        BGMVolume = value / 100f;
         Debug.Assert(sm != null, "NULL?!");
-        sm.SetVolume(volume);
+        sm.SetBGMVolume(BGMVolume);
     }
 
-    public float GetVolume() => volume;
+    public float GetBGMVolume() => BGMVolume;
+
+    public void SetSFXVolume(float value) {
+        SFXVolume = value / 100f;
+        Debug.Assert(sm != null, "NULL!?");
+        sm.SetSFXVolume(SFXVolume);
+    }
+
+    public float GetSFXVolume() => SFXVolume;
     public void SetSensitivity(float value)
     {
         sensitivity = value;
