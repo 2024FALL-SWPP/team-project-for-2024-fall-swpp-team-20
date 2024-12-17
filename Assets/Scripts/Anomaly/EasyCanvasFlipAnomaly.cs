e3a75559d8fa292c59569b7f680d1c9a14bb206e
Gunhee Kim <gunhee2001@snu.ac.kr>
2024-12-17 15:28:53 +0900

    fix EasyCanvasFlipAnomaly

--- Assets/Scripts/Anomaly/EasyCanvasFlipAnomaly.cs
+++ Assets/Scripts/Anomaly/EasyCanvasFlipAnomaly.cs
@@ -7,7 +7,7 @@ public class EasyCanvasFlipAnomaly : Anomaly
     public override void Apply(GameObject map)
     {
         GameObject Canvas = storage.giraffeCanvas;
-        Canvas.transform.rotation = Quaternion.Euler(0, -90, 0);
+        Canvas.transform.rotation = Quaternion.Euler(0, -90, 180);
         Canvas.transform.localPosition = new Vector3(-7.547f, 1.713f, 3.52f);
     }
 }
